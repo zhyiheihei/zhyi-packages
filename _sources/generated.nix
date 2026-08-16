023affd35f7a083727c4217fc5ce97f3a929d6ab
Zhyi <molishanguang@outlook.com>
2026-08-17 04:18:20 +0800

    auto: update packages

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -87,12 +87,12 @@
   };
   navdash = {
     pname = "navdash";
-    version = "v0.3.0";
+    version = "v0.3.1";
     src = fetchFromGitHub {
       owner = "zhyiheihei";
       repo = "navdash";
-      tag = "v0.3.0";
-      hash = "sha256-M3RxqaXokpp64Kr2NzTnsqLXOamDXFh4VdjjPVNXynM=";
+      tag = "v0.3.1";
+      hash = "sha256-PvWes72Bmzd2l8qg2N5xGi342zjMA4Ak7KYGDUWO0zg=";
     };
   };
   nexus-media = {
